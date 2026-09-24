0b37d5bc2988f4dae1dfd6ac8ec025a09dbe0016
Sebastian Noel <snoel.dev@gmail.com>
2026-09-23 22:59:38 -0400

    feat: add COLORS API endpoitns with external database config

---
+++ LAMPAPI/AddColor.php
@@ -0,0 +1,47 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	$config = require '/etc/colors/database.php';
+
+	$conn = new mysqli(
+		$config['host'],
+		$config['username'],
+		$config['password'],
+		$config['database']
+	);
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
